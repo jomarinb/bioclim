1ff542d5e2ece1a652f9a6fc4863c942a3efadd9
José Marín <jomarinb@users.noreply.github.com>
2017-09-19 23:11:32 -0600

    Initial commit
    * Add yeoman angular boilerplate

---
+++ web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bioclimDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bioclimDemoApp
+ */
+angular.module('bioclimDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bioclimDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bioclimDemoApp
+ */
+angular.module('bioclimDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
